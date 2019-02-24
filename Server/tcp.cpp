5e7ef1ec1a3cbc9744e02345d0af30b5e6e4fc93
Jens Lindström <02jli01@skola.falun.se>
2019-02-24 15:02:18 +0100

    Implemented port function of both classes

--- Server/tcp.cpp
+++ Server/tcp.cpp
@@ -14,11 +14,12 @@ tcp_server::~tcp_server()
 
 int tcp_server::port()
 {
-	return 0;
+	return accepting_port;
 }
 
 void tcp_server::port(int new_port)
 {
+	accepting_port = new_port;
 }
 
 bool tcp_server::check_error(int error_code)
@@ -54,11 +55,12 @@ tcp_client::tcp_client()
 
 int tcp_client::port()
 {
-	return 0;
+	return connection_port;
 }
 
 void tcp_client::port(int new_port)
 {
+	connection_port = new_port;
 }
 
 std::string tcp_client::ip()
