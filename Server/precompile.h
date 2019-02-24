d64a21efc288e65397b41de467b98eae681b8a3a
Jens Lindström <02jli01@skola.falun.se>
2019-02-24 14:53:42 +0100

    Renamed Files, Added a precompile file, Class header implemented
    "Server.cpp" && "Client.cpp" -> "main.cpp"

--- Server/precompile.h
+++ Server/precompile.h
@@ -1,2 +1,16 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <sstream>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <TlHelp32.h>
+#include <iphlpapi.h>
+#include <memory>
+#include <locale>
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment (lib, "ws2_32.lib")
---
+++ Server/tcp.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "precompile.h"
+
+class tcp_server {
+public:
+	tcp_server();
+	tcp_server(int);
+	~tcp_server();
+
+	int port();
+	void port(int);
+
+private:
+
+	int port = 0;
+	bool verbosity = true;
+
+	WSADATA socket_data;
+	fd_set client_set;
+	SOCKET main_socket;
+	SOCKET active_socket;
+	std::thread manager_thread;
+
+	bool check_error(int);
+	bool socket_listen(SOCKET&);
+	bool socket_bind(SOCKET&, int);
+	bool socket_startup(WSADATA&, SOCKET&);
+
+};
+
+class tcp_client {
+
+public:
+
+	tcp_client(std::string, int);
+	tcp_client();
+
+	int port();
+	void port(int);
+	std::string ip();
+	void ip(std::string);
+
+private:
+
+	WSADATA socket_data;
+	SOCKET connection_socket;
+	sockaddr_in socket_hint;
+	std::string ip_address;
+	int port;
+
+	bool socket_startup(WSADATA&, SOCKET&);
+	bool socket_connect(SOCKET&, sockaddr_in&);
+};
