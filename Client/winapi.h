470e4b266fcceaf9660adda225d5fb9def4e337e
Jens Lindström <02jli01@skola.falun.se>
2019-04-17 22:37:01 +0200

    Made registry branch in winapi.h

--- Client/winapi.h
+++ Client/winapi.h
@@ -3,7 +3,7 @@
 
 namespace winapi {
 	inline std::string wide2string(WCHAR* input, int size) {
-		char* ch = new char[size-1];
+		char* ch = new char[size - 1];
 		WideCharToMultiByte(CP_ACP, 0, input, -1, ch, size, NULL, NULL);
 		return std::string(ch);
 	}
@@ -20,11 +20,56 @@ namespace winapi {
 			{
 				RegQueryValueExW(keyHandle, L"ProductName", NULL, &Type, (LPBYTE)rgValue, &size);
 				RegCloseKey(keyHandle);
-				return wide2string(rgValue, size+1);
+				return wide2string(rgValue, size + 1);
 			}
 			else {
 				return "ERROR";
 			}
 		}
+
+		inline std::string windows_dir() {
+
+		}
+
 	}
+
+	namespace registry {
+		// Currently only works with REG_DWORD and REG_SZ
+		inline std::string RegValue(HKEY registry_hive, std::string key_name, std::string value) { 
+
+			// Get Registry Size and Type
+			unsigned long value_type{};
+			unsigned long value_size{};
+			if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, NULL, &value_size)) {
+
+				switch (value_type) {
+				case REG_DWORD:
+				{
+					DWORD buffer;
+					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, &buffer, &value_size)) {
+						return std::to_string(buffer);
+					}
+				}
+				case REG_SZ:
+				case REG_MULTI_SZ:
+				case REG_EXPAND_SZ:
+				{
+					std::vector<unsigned char> buffer(value_size);
+					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, buffer.data(), &value_size)) {
+						return std::string(buffer.begin(), buffer.end());
+					}
+				}
+				default:
+					break;
+				}
+			}
+			else {
+				std::cout << "Error: The System cannot find the registry entry!" << std::endl;
+			}
+
+			return "";
+
+		}
+	}
+
 }
