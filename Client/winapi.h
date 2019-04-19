ddd8ec90ee5c9470ed1393d8b3eafe7858964fd7
Jens Lindström <02jli01@skola.falun.se>
2019-04-19 11:59:52 +0200

    Upadted winapi.h

--- Client/winapi.h
+++ Client/winapi.h
@@ -2,51 +2,23 @@
 #include "..\Library\precompile.h"
 
 namespace winapi {
-	inline std::string wide2string(WCHAR* input, int size) {
-		char* ch = new char[size - 1];
-		WideCharToMultiByte(CP_ACP, 0, input, -1, ch, size, NULL, NULL);
-		return std::string(ch);
-	}
-
-	namespace windows {
-		inline std::string windows_version() {
-			HKEY keyHandle;
-			WCHAR rgValue[1024];
-			WCHAR fnlRes[1024];
-			DWORD size = 1023;
-			DWORD Type;
-
-			if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_QUERY_VALUE | KEY_WOW64_64KEY, &keyHandle) == ERROR_SUCCESS)
-			{
-				RegQueryValueExW(keyHandle, L"ProductName", NULL, &Type, (LPBYTE)rgValue, &size);
-				RegCloseKey(keyHandle);
-				return wide2string(rgValue, size + 1);
-			}
-			else {
-				return "ERROR";
-			}
-		}
-
-		inline std::string windows_dir() {
-
-		}
-
-	}
 
 	namespace registry {
 		// Currently only works with REG_DWORD and REG_SZ
-		inline std::string RegValue(HKEY registry_hive, std::string key_name, std::string value) { 
-
+		inline std::string GetValue(HKEY registry_hive, std::string key_name, std::string value) {
 			// Get Registry Size and Type
 			unsigned long value_type{};
 			unsigned long value_size{};
-			if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, NULL, &value_size)) {
+
+
+			LSTATUS ret = RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, &value_type, NULL, &value_size);
+			if (!ret) {
 
 				switch (value_type) {
 				case REG_DWORD:
 				{
 					DWORD buffer;
-					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, &buffer, &value_size)) {
+					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, &value_type, &buffer, &value_size)) {
 						return std::to_string(buffer);
 					}
 				}
@@ -55,7 +27,7 @@ namespace winapi {
 				case REG_EXPAND_SZ:
 				{
 					std::vector<unsigned char> buffer(value_size);
-					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, &value_type, buffer.data(), &value_size)) {
+					if (!RegGetValueA(registry_hive, key_name.c_str(), value.c_str(), RRF_RT_ANY | RRF_SUBKEY_WOW6464KEY, &value_type, buffer.data(), &value_size)) {
 						return std::string(buffer.begin(), buffer.end());
 					}
 				}
@@ -67,8 +39,35 @@ namespace winapi {
 				std::cout << "Error: The System cannot find the registry entry!" << std::endl;
 			}
 
-			return "";
+			return std::to_string(ret);
 
+		}
+		inline bool exists(HKEY hive, std::string key_name, std::string value) {
+			return RegGetValueA(hive, key_name.c_str(), value.c_str(), RRF_RT_ANY, NULL, NULL, NULL) != ERROR_FILE_NOT_FOUND;
+		}
+	}
+
+	namespace windows {
+		namespace info {
+			inline std::string windows_product() {
+				return registry::GetValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "productname");
+			}
+
+			inline std::string windows_root() {
+				return registry::GetValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "systemroot");
+			}
+
+			inline std::string windows_owner() {
+				return registry::GetValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "RegisteredOwner");
+			}
+
+			inline std::string windows_organization() {
+				return registry::GetValue(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "RegisteredOrganization");
+			}
+
+			inline std::string windows_architecture() {
+				return registry::GetValue(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", "PROCESSOR_ARCHITECTURE");
+			}
 		}
 	}
 
